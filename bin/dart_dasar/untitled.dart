f46803c41f57b97efe33bec137a9fa5e922e9595
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2022-03-07 12:13:18 +0700

    ignore this

---
+++ bin/dart_dasar/untitled.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+// void main() {
+//   print(numberToString(123456789));
+// }
+
+// String numberToString(int n) {
+//   return 'Hello ${n.toString()}';
+// }
+
+// void main() {
+//   stdout.write('Suhu sekarang dalam farenheit: ');
+//   var farenheit = num.parse(stdin.readLineSync()!);
+//   var celcius = (farenheit - 32) * 5 / 9;
+//   print('$farenheit = dalam celcius: $celcius');
+// }
+
+void main() {
+  List<String> fruits = <String>['Pisang', 'Jambu', 'Apel', 'Durian'];
+  var fruit = fruits.length - 1;
+  print(fruits[fruit]);
+}
